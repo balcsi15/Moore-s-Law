e409df39c6f6aaaba43613ab060cde56b23cd4cc
Alex Balázs <alex.balazs90@gmail.com>
2020-10-15 16:19:39 +0200

    The Law of Moorse

---
+++ Moores Law/Moores Law/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moores_Law
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, n;
+            float j = 0;
+            Console.WriteLine("Introduceti cresterea puteri de calcul dorite:");
+            n = int.Parse(Console.ReadLine());
+
+            while (i < n)
+            {
+
+                i = i * 2;
+                j = j + 2;
+            }
+
+            j = j + (j / 2);
+            j = j / 2;
+            Console.WriteLine("Timpul necesar este:" + " " + j + " " + "ani");
+            Console.ReadLine();
+        }
+    }
+}
